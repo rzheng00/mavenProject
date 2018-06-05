f9f982574fc76ae1a644e5156c6e4c5a62f1d279
rzheng00 <rick.zheng@hotmail.com>
2018-06-04 23:21:59 -0700

    # WARNING: head commit changed in the meantime
    test message in eclipse

--- src/test/java/test/rsTest.java
+++ src/test/java/test/rsTest.java
@@ -19,8 +19,8 @@ import static org.hamcrest.Matchers.*;
 
 //import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+//import java.util.List;
+//import java.util.Map;
 
 
 
